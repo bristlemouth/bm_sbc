b00f39ec2f5feaa1efc2ab1489c6cc040715a556
Victor Sowa <victor.sowa@sofarocean.com>
2026-07-16 11:20:53 -0700

    move version info into its own file

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,21 +2,6 @@
 #include "bm_log.h"
 #include "runtime.h"
 
-#ifndef BM_SBC_APP_NAME
-#define BM_SBC_APP_NAME "bm_sbc"
-#endif
-
-// Identity marker baked into read-only data at build time.
-// DFU validation scans the staging binary for this string before swapping,
-// ensuring a bm_sbc_gateway binary cannot be flashed onto a different app target.
-// The section attribute syntax differs between ELF (Linux) and Mach-O (macOS).
-#ifdef __linux__
-__attribute__((used, section(".rodata")))
-#else
-__attribute__((used))
-#endif
-static const char k_image_marker[] = "BM_SBC_IMAGE:" BM_SBC_APP_NAME;
-
 int main(int argc, char **argv) {
   bm_sbc_runtime_set_argv(argc, argv);
   int rc = bm_sbc_runtime_init(argc, argv, BM_SBC_APP_NAME);
---
+++ src/core/version_info.cpp
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "git_sha.h"
+
+#ifndef BM_SBC_APP_NAME
+#define BM_SBC_APP_NAME "bm_sbc"
+#endif
+
+#define MAX_VERSION_STR_LEN 96
+
+// Binary identity data baked in at build time.
+// DFU validation and the DFU host scanner both search for these in the binary.
+#ifdef __linux__
+__attribute__((used, section(".rodata")))
+#else
+__attribute__((used))
+#endif
+static const char k_image_marker[] = "BM_SBC_IMAGE:" BM_SBC_APP_NAME;
+
+#define VERSION_INFO_MAGIC UINT64_C(0xDF7F9AFDEC06627C)
+
+typedef struct {
+  uint64_t magic;           // Magic number to identify this as a valid versionInfo_t
+  uint32_t gitSHA;          // git SHA for image
+  uint8_t maj;              // Major version
+  uint8_t min;              // Minor version
+  uint8_t rev;              // revision
+  uint8_t hwVersion;        // Hardware version (0 for don't care)
+  uint32_t flags;           // Various flags (specified above)
+  uint16_t versionStrLen;   // Version string length
+  const char versionStr[MAX_VERSION_STR_LEN]; // Version string
+} __attribute__((packed)) versionInfo_t;
+
+#ifdef __linux__
+__attribute__((used, section(".rodata")))
+#else
+__attribute__((used))
+#endif
+static const versionInfo_t k_version_info = {
+  .magic         = VERSION_INFO_MAGIC,
+  .gitSHA        = BM_SBC_GIT_SHA,
+  .maj           = BM_SBC_VERSION_MAJOR,
+  .min           = BM_SBC_VERSION_MINOR,
+  .rev           = BM_SBC_VERSION_PATCH,
+  .hwVersion     = 0,
+  .flags         = 0,
+  .versionStrLen = 0,  // Not used for now
+  .versionStr    = "", // Not used for now
+};
